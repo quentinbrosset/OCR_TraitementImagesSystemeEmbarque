2c385ab9586690c28c511508c14d09aee781beee
quentinbrosset <brosset.quentin@gmail.com>
2025-12-09 23:02:06 +0100

    Retrait startup présent dans Azure

--- startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /home/site/wwwroot
-if [ "$APP_TYPE" = "streamlit" ]; then
-  streamlit run application/app.py --server.port 8000 --server.address 0.0.0.0
-else
-  # Default to API (for Azure Web App)
-  python -m uvicorn application.api:app --host 0.0.0.0 --port 8000
-fi 
